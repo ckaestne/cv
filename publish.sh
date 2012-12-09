9dd9c493e8a2e0b137f46307d4de958435e7b6ff
Christian Kaestner <kaestner@cs.cmu.edu>
2012-12-09 11:01:04 -0500

    update publish script to produce pdf and run test

--- publish.sh
+++ publish.sh
@@ -1,2 +1,8 @@
-sbt run
+sbt test
+sbt "run-main de.stner.cv.GenLatex"
+cd target/pdf
+pdflatex -interaction=nonstopmode cv.tex
+cd ../..
+sbt "run-main de.stner.cv.GenHtml"
+cp target/pdf/cv.pdf target/site/cv.pdf
 rsync -vzr target/site/ /afs/cs/user/ckaestne/www/
