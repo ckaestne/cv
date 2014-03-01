4cb683f66268377857254677a5cb22dc8034f2d6
Christian Kaestner <kaestner@cs.cmu.edu>
2014-03-01 09:48:02 -0500

    fix error

--- src/main/scala/CVPublications.scala
+++ src/main/scala/CVPublications.scala
@@ -2685,8 +2685,8 @@ understand and control its influence in program-comprehension experiments."""
             Seq(HNguyen, Kaestner, TNguyen),
             "Exploring Variability-Aware Execution for Testing Plugin-Based Web Applications",
             ICSE14,
+            ToAppear(),
             Map(PDF->PDFFile("icse14_varex.pdf")),
-            Map(),
             """
               In plugin-based systems, plugin conflicts may occur when two or more
               plugins interfere with one another, changing their expected
